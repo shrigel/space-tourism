e7546fb09a52afe1cd94dfe31d6cfacac6fdd26f
Zikra Daffa Saputra <zdsaputra@gmail.com>
2026-08-31 22:42:34 +0700

    implement technology page; implement hovering;

--- src/pages/Technology/Technology.jsx
+++ src/pages/Technology/Technology.jsx
@@ -1,11 +1,69 @@
-import Header from "../../components/Header/Header";
+import { useState } from "react";
 import "./Technology.scss";
+import Header from "../../components/Header/Header";
+import data from "../../assets/data.json";
 
 export default function Technology() {
+    const [technologyIndex, setTechnologyIndex] = useState(0);
+    const currentTechnology = data.technology[technologyIndex];
+
+    const images = import.meta.glob('../../assets/**/*.{png,webp,jpg}', {
+        eager: true,
+        import: 'default',
+    });
+
+    const getImageUrl = (path) => {
+        const cleanPath = path.replace(/^\.\//, '');
+        return images[`../../assets/${cleanPath}`];
+    };
+
     return (
         <div className="page--technology">
             <Header />
-            <h1>Technology</h1>
+
+            <main className="technology">
+                <div className="technology__title">
+                    <span className="technology__title-number">03</span>
+                    <span className="technology__title-text">SPACE LAUNCH 101</span>
+                </div>
+
+                <div className="technology__container">
+                    <div className="technology__image">
+                        <picture>
+                            <source media="(min-width: 1024px)" srcset={getImageUrl(currentTechnology.images.portrait)} />
+                            <img src={getImageUrl(currentTechnology.images.landscape)} alt="" />
+                        </picture>
+                    </div>
+
+                    <div className="technology__content" role="tabpanel" aria-label={currentTechnology.name}>
+                        <div className="technology__pagination">
+                            {data.technology.map((technology, index) => {
+                                const isActive = technologyIndex === index;
+
+                                return (
+                                    <button key={technology.name}
+                                        role="tab"
+                                        aria-selected={technologyIndex === index}
+                                        aria-label={`Slide ${index + 1}: ${technology.name}`}
+                                        className={`technology__pagination-number ${isActive ? "active" : ""}`}
+                                        onClick={() => setTechnologyIndex(index)}>
+                                        {index + 1}
+                                    </button>
+                                )
+                            })}
+                        </div>
+
+                        <div className="technology__explanation">
+                            <div className="technology__info">
+                                <span>THE TERMINOLOGY</span>
+                                <span className="technology__name">{currentTechnology.name}</span>
+                            </div>
+
+                            <p className="technology__description">{currentTechnology.description}</p>
+                        </div>
+                    </div>
+                </div>
+            </main>
         </div>
     )
 }
